ae4d29aa93be0302bfc765d807dc1114efe458af
Mario <mqs@melix.org>
2017-02-16 14:06:37 +0100

    finish adding client code in jquery

---
+++ BethanyPieShop/Controllers/PieDataController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class PieDataController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public PieDataController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<PieViewModel> LoadMorePies()
+        {
+            IEnumerable<Pie> dbPies = null;
+
+            dbPies = this.pieRepository.Pies.OrderBy(p => p.PieId).Take(10);
+
+            List<PieViewModel> pies = new List<PieViewModel>();
+
+            foreach (var dbPie in dbPies)
+            {
+                pies.Add(MapDbPieToPieViewModel(dbPie));
+            }
+            return pies;
+        }
+
+        private PieViewModel MapDbPieToPieViewModel(Pie dbPie)
+        {
+            return new PieViewModel()
+            {
+                PieId = dbPie.PieId,
+                Name = dbPie.Name,
+                Price = dbPie.Price,
+                ShortDescription = dbPie.ShortDescription,
+                ImageThumbnailUrl = dbPie.ImageThumbnailUrl
+            };
+        }
+
+    }
+}
